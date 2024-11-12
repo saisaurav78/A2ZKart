813e73b218402ae38ea37a78001b71523568c49d
Sai_Sowrav <93769816+saisaurav78@users.noreply.github.com>
2024-11-12 12:29:03 +0530

    Update main.jsx

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import  { CartContextProvider } from './Contexts/CartContext.jsx';
-import { SearchContextProvider } from './Contexts/searchContext.jsx';
+import { SearchContextProvider } from './Contexts/SearchContext.jsx';
 import { VisibilityContextProvider } from './Contexts/VisibilityContext.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
